2523a6e3124402ba7130152570db4345fcaccc39
Sorrawich Yooboon <sorrawich.y@gmail.com>
2023-12-22 22:40:36 +0700

    data type

--- Program.cs
+++ Program.cs
@@ -6,9 +6,20 @@ namespace CSharpFundamental
     {
         static void Main(string[] args)
         {
-            Console.Write("Hey");
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("\tBro code");
+            int x;
+            x = 123;
+            Console.WriteLine(x);
+            int y = 321;
+            double height = 174.5;
+            bool alive = true;
+            char myChar = 'A';
+            string myName = "John";
+            string myName2 = "John2";
+            Console.WriteLine("Your number is " + height);
+            Console.WriteLine("Are you alive? " + alive);
+            Console.WriteLine("My name is " + myName);
+            Console.WriteLine("My name is " + myName2);
+            Console.WriteLine("My name is {0} and {1} and {2}", myName, myName2, myChar);
         }
     }
 }
